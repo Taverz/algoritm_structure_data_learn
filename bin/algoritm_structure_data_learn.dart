c4559e984158dd54c1eef3ee86c8ac5ad00b9094
NikitaTaverz <nikita3kovalev3@gmail.com>
2024-05-01 17:21:55 +0300

    first commit

---
+++ bin/algoritm_structure_data_learn.dart
@@ -0,0 +1,5 @@
+// import 'package:algoritm_structure_data_learn/algoritm_structure_data_learn.dart' as algoritm_structure_data_learn;
+
+// void main(List<String> arguments) {
+//   print('Hello world: ${algoritm_structure_data_learn.calculate()}!');
+// }
---
+++ test/algoritm_structure_data_learn_test.dart
@@ -0,0 +1,8 @@
+import 'package:algoritm_structure_data_learn/algoritm_structure_data_learn.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
